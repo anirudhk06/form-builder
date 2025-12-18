d6978db44c6015539e2ed650205b5763439978b2
anirudhk06 <anirudhani300@gmail.com>
2025-12-18 09:37:24 +0530

    custom pagination and filterset added

--- src/apps/users/views.py
+++ src/apps/users/views.py
@@ -1,12 +1,24 @@
 from django.contrib.auth import get_user_model
-from rest_framework import generics
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import generics, status
+from rest_framework.filters import SearchFilter
+from rest_framework.response import Response
+
 from .serializers import UserCreateSerializer, UserListSerializer
 from .permissions import IsStaffUser
+from .filters import UserFilter
+from core.utils.pagination import CustomLimitPagination
 
 USER = get_user_model()
 class UserListCreateView(generics.ListCreateAPIView):
     serializer_class = UserCreateSerializer
+    pagination_class = CustomLimitPagination
     permission_classes = [IsStaffUser]
+    search_fields = [
+        "email"
+    ]
+    filter_backends = [SearchFilter, DjangoFilterBackend]
+    filterset_class = UserFilter
 
     def get_queryset(self):
         return USER.objects.filter(
@@ -19,7 +31,16 @@ class UserListCreateView(generics.ListCreateAPIView):
         return super().get_serializer_class()
     
     def list(self, request, *args, **kwargs):
-        return super().list(request, *args, **kwargs)
+        qs = self.filter_queryset(self.get_queryset())
+        page = self.paginate_queryset(qs)
+        users = self.get_serializer(page, many=True).data
+        return Response({**self.paginator.get_pagination_details(), "users": users})
     
     def create(self, request, *args, **kwargs):
-        return super().create(request, *args, **kwargs)
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response({
+            "message": "User created successfully"
+        }, status=status.HTTP_201_CREATED)
